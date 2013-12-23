6d85c7089eee972eea8644b67ab0a7c077a9af04
Administrator <a.bosonchenko@possoftware.com.ua>
2013-12-23 17:31:03 +0200

    loader

--- reload_cust.sh
+++ reload_cust.sh
@@ -1,2 +1,5 @@
- php app/console doctrine:database:create
-  php app/console doctrine:schema:update --force
+sudo chmod -R 777 app
+php app/console doctrine:database:create
+php app/console doctrine:schema:update --force
+php app/console doctrine:fixtures:load
+php app/console assets:install web --symlink
