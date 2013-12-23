57c26cd8ea51bcd76bc74ed46cf78f214f4d8118
Administrator <a.bosonchenko@possoftware.com.ua>
2013-12-23 16:52:38 +0200

    film rating counter fix

---
+++ src/Andboson/FilmzBundle/EventListener/FilmRatingUpdater.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Andboson\FilmzBundle\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Andboson\FilmzBundle\Entity\Comments;
+use Andboson\FilmzBundle\Entity\Film;
+
+class FilmRatingUpdater
+{
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $entityManager = $args->getEntityManager();
+        $film = $entityManager->getRepository('AndbosonFilmzBundle:Film')->find( $entity->getFilm() );
+
+
+
+        // perhaps you only want to act on some "Product" entity
+        if ($entity instanceof Comments) {
+
+            $query = $entityManager
+                ->createQuery('
+            SELECT c FROM AndbosonFilmzBundle:Comments c
+            WHERE c.film = :film'
+                )->setParameters(Array('film' => $entity->getFilm() ));
+
+            $comments = $query->getArrayResult();
+
+            $rating = $entity->getRate();
+            $count = count( $comments ) + 1;
+            foreach( $comments as $comment ){
+                $rating += $comment['rate'];
+            }
+
+            $averageRating = $rating / $count;
+            $film->setRating( $averageRating );
+            $entityManager->persist($film);
+            $entityManager->flush();
+
+        }
+    }
+}
